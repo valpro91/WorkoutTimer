6c4f2207e84cfc2f89fe6493de2a8de8d9d7ea41
Valentin <valentin.prossliner@gmail.com>
2024-04-28 14:22:11 -0400

    Initial Commit

---
+++ Workout Timer/Workout_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_TimerApp.swift
+//  Workout Timer
+//
+//  Created by Valentin Prossliner on 28/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Workout_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
